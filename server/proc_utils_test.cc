e42eb65fb4977439d14e706f01b76ac8a1f1cdbf
Daniel Muino <dmuino@netflix.com>
2020-09-28 10:35:55 -0700

    Clean up directory structure
    Move resources to test-resources to make it obvious what the files are
    used for. Remove Netflix-specific start script + sample config file.
    
    Add missing (empty) BUILD file to mark third_party directory as a
    location for build scripts

--- server/proc_utils_test.cc
+++ server/proc_utils_test.cc
@@ -8,9 +8,9 @@ using spectatord::udp_info_t;
 
 TEST(ProcUtils, max_buffer) {
   // test repeated calls which should only read from the file once
-  EXPECT_EQ(max_buffer_size("resources/rmem_max"), 65536);
-  EXPECT_EQ(max_buffer_size("resources/rmem_max"), 65536);
-  EXPECT_EQ(max_buffer_size("resources/rmem_max"), 65536);
+  EXPECT_EQ(max_buffer_size("test-resources/rmem_max"), 65536);
+  EXPECT_EQ(max_buffer_size("test-resources/rmem_max"), 65536);
+  EXPECT_EQ(max_buffer_size("test-resources/rmem_max"), 65536);
 
   // get default when we can't read the file
   constexpr auto kDefault = 16777216;
@@ -18,17 +18,17 @@ TEST(ProcUtils, max_buffer) {
 }
 
 TEST(ProcUtils, udp_info) {
-  auto res = udp_info(1234, "resources/net_udp_1");
+  auto res = udp_info(1234, "test-resources/net_udp_1");
   ASSERT_TRUE(res.has_value());
   EXPECT_EQ(res->num_dropped, 166662);
   EXPECT_EQ(res->rx_queue_bytes, 0);
 
-  res = udp_info(1235, "resources/net_udp_1");
+  res = udp_info(1235, "test-resources/net_udp_1");
   ASSERT_TRUE(res.has_value());
   EXPECT_EQ(res->num_dropped, 10);
   EXPECT_EQ(res->rx_queue_bytes, 0x100);
 
-  res = udp_info(1, "resources/net_udp_1");
+  res = udp_info(1, "test-resources/net_udp_1");
   ASSERT_FALSE(res.has_value());
 
   res = udp_info(1234, "/does-not-exist-dir/does-not-exist-file");
