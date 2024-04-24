02c19b3a743671fb5be81aed30f7e78d403831ca
Matthew Johnson <mjjohnson33@gmail.com>
2024-04-23 21:50:08 -0400

    move admin server to localhost (#85)

--- admin/admin_server.h
+++ admin/admin_server.h
@@ -6,6 +6,9 @@
 
 namespace admin {
 
+using namespace Poco::Net;
+using namespace spectator;
+
 std::string join(std::vector<std::string> const& strings, const std::string& delimiter);
 spectator::Id parse_id(const std::string& id_str);
 
@@ -24,28 +27,28 @@ class RequestHandler: public Poco::Net::HTTPRequestHandler {
   };
 
  public:
-  explicit RequestHandler(spectator::Registry &r): registry{r} {}
-  void handleRequest(Poco::Net::HTTPServerRequest &, Poco::Net::HTTPServerResponse &) override;
+  explicit RequestHandler(Registry &r): registry{r} {}
+  void handleRequest(HTTPServerRequest &, HTTPServerResponse &) override;
 };
 
-class RequestHandlerFactory: public Poco::Net::HTTPRequestHandlerFactory {
-  spectator::Registry &registry;
+class RequestHandlerFactory: public HTTPRequestHandlerFactory {
+  Registry &registry;
 
  public:
-  explicit RequestHandlerFactory(spectator::Registry &r): registry{r} {}
-  Poco::Net::HTTPRequestHandler* createRequestHandler(const Poco::Net::HTTPServerRequest &) override {
+  explicit RequestHandlerFactory(Registry &r): registry{r} {}
+  HTTPRequestHandler* createRequestHandler(const HTTPServerRequest &) override {
     return new admin::RequestHandler(this->registry);
   }
 };
 
 class AdminServer {
-  std::shared_ptr<Poco::Net::HTTPServer> instance;
-  spectator::Registry &registry;
+  std::shared_ptr<HTTPServer> instance;
+  Registry &registry;
 
  public:
-  AdminServer(spectator::Registry &r, int p):
+  AdminServer(Registry &r, int p):
     registry{r},
-    instance{std::make_shared<Poco::Net::HTTPServer>(new RequestHandlerFactory(r), p)} {}
+    instance{std::make_shared<HTTPServer>(new RequestHandlerFactory(r), SocketAddress("localhost", p), new HTTPServerParams)} {}
   void Start() { instance->start(); }
   void Stop() { instance->stop(); }
 };
