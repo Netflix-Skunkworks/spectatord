2a527d2bdefd107ebc00686b3ca6b71a1966d489
Matthew Johnson <matthewj@netflix.com>
2024-05-21 17:46:30 -0400

    monotonic counters should store values as uint64_t
    We have use cases in the environment, where monotonic counters are stored as
    unit64_t values, particularly for Open Connect Appliances. Since the monotonic
    counter data types in spectatord are convenience wrappers that help users avoid
    the need to calculate their own deltas, they should be uint64_t types here. The
    actual deltas that are calculated and sent to the backend are doubles, which
    matches the backend data type used by Atlas. This is acceptable, because the
    deltas are expected to be much smaller values.
    
    Some of the newer thin clients, such as spectator-go, specify the uint64 data
    type for monotonic counters.
    
    Note that the previous implementation for monotonic counters did not handle
    rollovers correctly, and this change fixes that behavior for both types.

--- spectator/monotonic_counter.h
+++ spectator/monotonic_counter.h
@@ -9,12 +9,13 @@ class MonotonicCounter : public Meter {
   explicit MonotonicCounter(Id id) noexcept;
   void Measure(Measurements* results) const noexcept;
 
-  void Set(double amount) noexcept;
+  void Set(uint64_t amount) noexcept;
   auto Delta() const noexcept -> double;
 
  private:
   mutable std::unique_ptr<Id> count_id_;
-  mutable std::atomic<double> value_;
-  mutable std::atomic<double> prev_value_;
+  mutable std::atomic<bool> init_;
+  mutable std::atomic<uint64_t> value_;
+  mutable std::atomic<uint64_t> prev_value_;
 };
 }  // namespace spectator
