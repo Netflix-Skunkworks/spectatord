0e2646d4aef26a73cb00937cf07ae15a6a68e60e
Matthew Johnson <mjjohnson33@gmail.com>
2024-12-08 16:42:58 -0500

    skip setting CC and CXX when building in CentOS (#115)

--- build.sh
+++ build.sh
@@ -34,9 +34,11 @@ if [[ "$1" == "clean" ]]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # these switches are necessary for internal centos builds
-  if [[ -z "$CC" ]]; then export CC=gcc-13; fi
-  if [[ -z "$CXX" ]]; then export CXX=g++-13; fi
+  source /etc/os-release
+  if [[ "$NAME" == "Ubuntu" ]]; then
+    if [[ -z "$CC" ]]; then export CC=gcc-13; fi
+    if [[ -z "$CXX" ]]; then export CXX=g++-13; fi
+  fi
 fi
 
 if [[ ! -f "$HOME/.conan2/profiles/default" ]]; then
