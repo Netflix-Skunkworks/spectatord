35f2dcef9baecb1989de0be480d67f8163de8b27
Everett Badeaux <everettc1810@gmail.com>
2024-12-16 11:40:17 -0600

    Compressed_Buffer_Improvements (#119)
    Add Another Unit Test
    
    CompressedBufferImprovements

--- spectator/compressed_buffer.h
+++ spectator/compressed_buffer.h
@@ -18,74 +18,40 @@ class CompressedBuffer {
   static constexpr size_t kDefaultChunkSizeInput = 256 * 1024;
   static constexpr size_t kDefaultOutSize = 256 * 1024;
   static constexpr size_t kDefaultChunkSizeOutput = 32 * 1024;
+
   explicit CompressedBuffer(size_t chunk_size_input = kDefaultChunkSizeInput,
                             size_t out_size = kDefaultOutSize,
                             size_t chunk_size_output = kDefaultChunkSizeOutput);
   CompressedBuffer(const CompressedBuffer&) = delete;
   CompressedBuffer(CompressedBuffer&&) = default;
   auto operator=(const CompressedBuffer&) -> CompressedBuffer& = delete;
-  auto operator=(CompressedBuffer &&) -> CompressedBuffer& = delete;
+  auto operator=(CompressedBuffer&&) -> CompressedBuffer& = delete;
 
   ~CompressedBuffer();
 
   auto Init() -> void;
 
-  auto Append(std::string_view s) -> void {
+  /**
+   * Append Templated Types to an internal buffer
+   *
+   * A variadic function that accepts a flexible number of arguments of either strings (std::string
+   * or std::string_view) or (uint8_t). It checks that the types are valid at compile time, and
+   * forwards them to an internal appending function for processing. We have statically asserted
+   * that all types in the parameter pack are either string_view or uint8_t
+   *
+   * @param args is a parameter pack of forwarding references that can accept a variable number of
+   * arguments of any type.
+   *
+   */
+  template <typename... Args>
+  auto Append(Args&&... args) -> void {
+    static_assert(
+        ((std::is_convertible_v<std::decay_t<Args>, std::string_view> ||
+          std::is_same_v<std::decay_t<Args>, uint8_t>) &&
+         ...),
+        "Invalid argument types. Only std::string, std::string_view, or uint8_t are allowed.");
     assert(init_);
-    std::copy(s.begin(), s.end(), std::back_inserter(cur_));
-    maybe_compress();
-  }
-
-  auto Append(uint8_t b) -> void {
-    assert(init_);
-    cur_.push_back(b);
-    maybe_compress();
-  }
-
-  auto Append(uint8_t b1, uint8_t b2) -> void {
-    assert(init_);
-    cur_.push_back(b1);
-    cur_.push_back(b2);
-    maybe_compress();
-  }
-
-  auto Append(uint8_t b1, uint8_t b2, uint8_t b3) -> void {
-    assert(init_);
-    cur_.push_back(b1);
-    cur_.push_back(b2);
-    cur_.push_back(b3);
-    maybe_compress();
-  }
-
-  auto Append(uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4) -> void {
-    assert(init_);
-    cur_.push_back(b1);
-    cur_.push_back(b2);
-    cur_.push_back(b3);
-    cur_.push_back(b4);
-    maybe_compress();
-  }
-
-  auto Append(uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, uint8_t b5)
-      -> void {
-    assert(init_);
-    cur_.push_back(b1);
-    cur_.push_back(b2);
-    cur_.push_back(b3);
-    cur_.push_back(b4);
-    cur_.push_back(b5);
-    maybe_compress();
-  }
-
-  auto Append(uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, uint8_t b5,
-              uint8_t b6) {
-    assert(init_);
-    cur_.push_back(b1);
-    cur_.push_back(b2);
-    cur_.push_back(b3);
-    cur_.push_back(b4);
-    cur_.push_back(b5);
-    cur_.push_back(b6);
+    AppendImpl(std::forward<Args>(args)...);
     maybe_compress();
   }
 
@@ -105,6 +71,19 @@ class CompressedBuffer {
   auto maybe_compress() -> void;
 
   auto deflate_chunk(size_t chunk_size, int flush) -> int;
+
+  template <typename... Args>
+  auto AppendImpl(Args&&... args) -> void {
+    if constexpr ((std::is_convertible_v<std::decay_t<Args>, std::string_view> && ...)) {
+      static_assert(sizeof...(args) == 1, "Only one string-like argument is allowed.");
+      (cur_.insert(cur_.end(), std::forward<Args>(args).begin(), std::forward<Args>(args).end()),
+       ...);
+    } else if constexpr ((std::is_same_v<std::decay_t<Args>, uint8_t> && ...)) {
+      static_assert(sizeof...(args) >= 1 && sizeof...(args) <= 6,
+                    "The number of uint8_t arguments must be between 1 and 6.");
+      (cur_.push_back(std::forward<Args>(args)), ...);
+    }
+  }
 };
 
 }  // namespace spectator
