01b78b8e2cb8beffa329c8c0ae253c85571bf077
Everett Badeaux <ebadeaux@netflix.com>
2026-01-26 21:21:15 -0600

    Revert "Add systemd socket activation support (#130)"
    This reverts commit 927446257c1f96740ec96191bd6f67bb932aa95f.

--- server/local_server.h
+++ server/local_server.h
@@ -7,16 +7,9 @@ namespace spectatord {
 
 class LocalServer {
  public:
-  // Create a LocalServer that binds to a new socket at the given path
   // NOLINTNEXTLINE(google-runtime-references)
   LocalServer(asio::io_context& io_context, std::string_view path,
               handler_t handler);
-
-  // Create a LocalServer using an existing socket file descriptor (systemd activation)
-  // NOLINTNEXTLINE(google-runtime-references)
-  LocalServer(asio::io_context& io_context, int socket_fd,
-              handler_t handler);
-
   void Start();
 
  private:
